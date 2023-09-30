2851b702380dcdebca3833e74cd2671fb9a7d1ad
abbyjo <abbyjo225@gmail.com>
2023-09-30 18:05:00 -0400

    Added GET routes for tag api

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,38 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const allTags = await Tag.findAll({
+      include: [{ model: Product }, { model: ProductTag }]
+    });
+    res.status(200).json(allTags);
+    
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const oneTag = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }, { model: ProductTag }]
+    });
+
+    if (!oneTag) {
+      res.status(404).json({message: 'Unable to find tag with this ID :-('});
+      return;
+    }
+
+    res.status(200).json(oneTag);
+
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.post('/', (req, res) => {
