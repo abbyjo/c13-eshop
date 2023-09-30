aba56d77a72afadfa0afd35df1f90bc1d56b8887
abbyjo <abbyjo225@gmail.com>
2023-09-30 17:47:14 -0400

    Added GET routes for products api

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,38 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try{
+    const allProducts = await Product.findAll({
+      include: [{model:Category}, {model: Tag}]
+    });
+    res.status(200).json(allProducts);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const myProduct = await Product.findByPk(req.params.id, {
+      include: [{model:Category}, {model: Tag}]
+    });
+
+    if (!myProduct) {
+      res.status(404).json({message: 'Unable to find product with this ID :-('});
+      return;
+    }
+
+    res.status(200).json(myProduct);
+
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 // create new product
