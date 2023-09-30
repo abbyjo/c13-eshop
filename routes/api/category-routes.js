9d99923b4a3d7d4605d9a8d981e62b2da1c78c18
abbyjo <abbyjo225@gmail.com>
2023-09-30 17:57:18 -0400

    Added GET routes for category api

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,38 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const allCats = await Category.findAll({
+      include: [{ model: Product }]
+    });
+    res.status(200).json(allCats);
+    
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const oneCat = await Category.findByPk(req.params.id, {
+      include: [{ model:Product }]
+    });
+
+    if (!oneCat) {
+      res.status(404).json({message: 'Unable to find category with this ID :-('});
+      return;
+    }
+
+    res.status(200).json(oneCat);
+
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.post('/', (req, res) => {
